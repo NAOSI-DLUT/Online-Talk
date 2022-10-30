eb7bf0b31c71baf3fbf8919b05efbc5964044291
BeautyyuYanli <beautyyuyanli@gmail.com>
2022-10-30 19:50:01 +0800

    sutato

--- NAOSI-4-web前端简介/backend/main.py
+++ NAOSI-4-web前端简介/backend/main.py
@@ -1,20 +1,31 @@
-from urllib import response
 from flask import Flask
-from flask import make_response
+from flask import make_response, request
 app = Flask(__name__)
 
-val = 0
+messages = []
 
-@app.route("/check")
+
+@app.route("/get_messages")
 def check():
-    res = make_response(str(val))
+    res = make_response("\n".join([x[0] + " " + x[1] for x in messages]))
     res.headers.add("Access-Control-Allow-Origin", "*")
     return res
 
-@app.route("/add_one")
+
+@app.route("/add_message")
 def add_one():
-    global val
-    val += 1
-    res = make_response("", 204)
+    global messages
+    name, msg = request.args.get("name"), request.args.get("msg")
+    if len(name)+len(msg) > 2048:
+        res = make_response("Too long")
+    else:
+        messages.append((name, msg))
+        if len(messages) > 64:
+            messages.pop(0)
+        res = make_response("OK")
     res.headers.add("Access-Control-Allow-Origin", "*")
     return res
+
+
+if __name__ == '__main__':
+    app.run()
